c17511fae11e2631470bf1a1b57a6c9bcf5fa33c
Timothy <timh1203@gmail.com>
2018-01-30 20:44:41 -0500

    Add stylings and neutrality colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,87 +8,98 @@ let seven = document.querySelector("#seven");
 let eight = document.querySelector("#eight");
 let nine = document.querySelector("#nine");
 let zero = document.querySelector("#zero");
+let viewScreen = document.querySelector(".calculator__view");
 let frontstage = document.querySelector(".calculator__view__screen");
 let backstage = "";
 
 // Click Listeners
 zero.addEventListener("click", (event) => {
   backstage += 0;
-  frontstage.innerHTML = backstage;
+  frontstage.append("0");
   console.log(backstage);
 })
 one.addEventListener("click", (event) => {
   backstage += 1;
-  frontstage.innerHTML = backstage;
+  frontstage.append("1");
   console.log(backstage);
 })
 two.addEventListener("click", (event) => {
   backstage += 2;
-  frontstage.innerHTML = backstage;
+  frontstage.append("2");
   console.log(backstage);
 })
 three.addEventListener("click", (event) => {
   backstage += 3;
-  frontstage.innerHTML = backstage;
+  frontstage.append("3");
   console.log(backstage);
 })
 four.addEventListener("click", (event) => {
   backstage += 4;
-  frontstage.innerHTML = backstage;
+  frontstage.append("4");
   console.log(backstage);
 })
 five.addEventListener("click", (event) => {
   backstage += 5;
-  frontstage.innerHTML = backstage;
+  frontstage.append("5");
   console.log(backstage);
 })
 six.addEventListener("click", (event) => {
   backstage += 6;
-  frontstage.innerHTML = backstage;
+  frontstage.append("6");
   console.log(backstage);
 })
 seven.addEventListener("click", (event) => {
   backstage += 7;
-  frontstage.innerHTML = backstage;
+  frontstage.append("7");
   console.log(backstage);
 })
 eight.addEventListener("click", (event) => {
   backstage += 8;
-  frontstage.innerHTML = backstage;
+  frontstage.append("8");
   console.log(backstage);
 })
 nine.addEventListener("click", (event) => {
   backstage += 9;
-  frontstage.innerHTML = backstage;
+  frontstage.append("9");
   console.log(backstage);
 })
 add.addEventListener("click", (event) => {
   backstage += "+";
-  frontstage.innerHTML = backstage;
+  frontstage.append("+");
   console.log(backstage);
 })
 subtract.addEventListener("click", (event) => {
   backstage += "-";
-  frontstage.innerHTML = backstage;
+  frontstage.append("-");
   console.log(backstage);
 })
 multiply.addEventListener("click", (event) => {
   backstage += "*";
-  frontstage.innerHTML = backstage;
+  frontstage.append("×");
   console.log(backstage);
 })
 divide.addEventListener("click", (event) => {
   backstage += "/";
-  frontstage.innerHTML = backstage;
+  frontstage.append("÷");
   console.log(backstage);
 })
 decimal.addEventListener("click", (event) => {
   backstage += ".";
-  frontstage.innerHTML = backstage;
+  frontstage.append(".");
   console.log(backstage);
 })
 equal.addEventListener("click", (event) => {
-  console.log("equal button pressed");
+  backstage = eval(backstage);
+  frontstage.innerHTML = backstage;
+  if (Math.sign(backstage) === 1) {
+    viewScreen.setAttribute("style","background: green;");
+  } 
+  else if (Math.sign(backstage) === -1) {
+    viewScreen.setAttribute("style","background: red;");
+  } else if (Math.sign(backstage) === 0) {
+    viewScreen.setAttribute("style", "background: yellow; color: black;")
+  }
+  console.log(backstage);
 })
 backspace.addEventListener("click", (event) => {
   backstage = backstage.slice(0, -1);
@@ -97,91 +108,111 @@ backspace.addEventListener("click", (event) => {
 })
 clear.addEventListener("click", (event) => {
   backstage = "";
-  frontstage.innerHTML = backstage;
+  frontstage.innerHTML = "";
+  viewScreen.setAttribute("style","background: none; color: none;");
+  console.log(backstage);
 })
 
 // Keypress Listeners
-document.addEventListener('keypress', (event) => {
-  console.log(event.key);
+document.addEventListener('keydown', (event) => {
   switch (event.key) {
     case "0":
-      frontstage.innerHTML += "0";
       backstage += 0;
+      frontstage.append("0");
       break;
     case "1":
-      frontstage.innerHTML += "1";
       backstage += 1;
+      frontstage.append("1");
       console.log(backstage);
       break;
     case "2":
-      frontstage.innerHTML += "2";
       backstage += 2;
+      frontstage.append("2");
       console.log(backstage);
       break;
     case "3":
-      frontstage.innerHTML += "3";
       backstage += 3;
+      frontstage.append("3");
       console.log(backstage);
       break;
     case "4":
-      frontstage.innerHTML += "4";
-      backstage += 4;
+    backstage += 4;
+      frontstage.append("4");
       console.log(backstage);
       break;
     case "5":
-      frontstage.innerHTML += "5";
       backstage += 5;
+      frontstage.append("5");
       console.log(backstage);
       break;
     case "6":
-      frontstage.innerHTML += "6";
       backstage += 6;
+      frontstage.append("6");
       console.log(backstage);
       break;
     case "7":
-      frontstage.innerHTML += "7";
       backstage += 7;
+      frontstage.append("7");
       console.log(backstage);
       break;
     case "8":
-      frontstage.innerHTML += "8";
       backstage += 8;
+      frontstage.append("8");
       console.log(backstage);
       break;
     case "9":
-      frontstage.innerHTML += "9";
       backstage += 9;
+      frontstage.append("9");
       console.log(backstage);
       break;
     case "+":
-    frontstage.innerHTML += "+";
       backstage += "+";
+      frontstage.append("+");
       break;
     case "-":
-      frontstage.innerHTML += "-";
       backstage += "-";
+      frontstage.append("-");
       break;
     case "*":
-      frontstage.innerHTML += "×";
       backstage += "*";
+      frontstage.append("×");
       break;
     case "/":
-      frontstage.innerHTML += "÷";
       backstage += "/";
+      frontstage.append("÷");
       break;
     case ".":
-      frontstage.innerHTML += ".";
       backstage += ".";
+      frontstage.append(".");
       break;
     case "Enter":
-      console.log("Enter");
+      backstage = eval(backstage);
+      frontstage.innerHTML = backstage;
+      if (Math.sign(backstage) === 1) {
+        viewScreen.setAttribute("style","background: green;");
+      } 
+      else if (Math.sign(backstage) === -1) {
+        viewScreen.setAttribute("style","background: red;");
+      } else if (Math.sign(backstage) === 0) {
+        viewScreen.setAttribute("style", "background: yellow; color: black;")
+      }
       break;
     case "Backspace":
-      console.log("Backspace");
+      backstage = backstage.slice(0, -1);
+      frontstage.innerHTML = backstage;
       break;
     case "Clear":
-      frontstage.innerHTML = "";
       backstage = "";
+      frontstage.innerHTML = "";
+      viewScreen.setAttribute("style","background: none; color: none;");
       break;
   }
 });
+
+window.onkeydown = (event) => {
+  if ( event.keyCode == 27 ) {
+    backstage = "";
+    frontstage.innerHTML = "";
+    viewScreen.setAttribute("style","background: none; color: none;");
+  }
+};
